27c885e503050f19efeea11634eddb4f0f6fa115
nota2k <nelly.babillon@gmail.com>
2024-06-13 01:00:51 +0200

    Tous les utilisateurs

--- senscritique_scrap/senscritique_scrap/spiders/senscritique.py
+++ senscritique_scrap/senscritique_scrap/spiders/senscritique.py
@@ -5,12 +5,40 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
+class MembresSpider(scrapy.Spider):
+    name = "membres"
+    allowed_domains = ["www.senscritique.com"]
+    start_urls = ["https://www.senscritique.com/nota2k/contacts"]
+    
+    def parse(self, response):
+        for membres in response.css('div.sc-74f141aa-0'):
+            link = membres.css('a::attr(href)')[1].get()
+            link_split = re.sub('/', '', link)
+            user = link_split
+            
+            yield {
+                    'username': membres.css("::text").get(),
+                    'link': user,
+                    'thmb': membres.css('a::attr(data-srcname)').get(),
+                    
+            }
 
 class SenscritiqueSpider(scrapy.Spider):
     name = "senscritique"
-    allowed_domains = ["www.senscritique.com"]
-    user = "nota2k"
-    start_urls = [f"https://www.senscritique.com/{user}/collection?action=WISH"]
+
+    def start_requests(self):
+        url = "https://www.senscritique.com/nota2k/contacts"
+        yield scrapy.Request(url, self.parse_users)
+
+    def parse_users(self, response):
+        users = []
+        for membres in response.css('div.sc-74f141aa-0'):
+            link = membres.css('a::attr(href)')[1].get()
+            link_split = re.sub('/', '', link)
+            users.append(link_split)
+
+        for user in users:
+            yield scrapy.Request(f"https://www.senscritique.com/{user}/collection?action=WISH", self.parse)
     
     def parse(self, response):
         for index, film in enumerate(response.css('div.sc-7d656c84-1')):
@@ -43,22 +71,7 @@ class SenscritiqueSpider(scrapy.Spider):
                 yield response.follow(pagination, callback=self.parse)
                 next_page += 1
 
-class MembresSpider(scrapy.Spider):
-    name = "membres"
-    allowed_domains = ["www.senscritique.com"]
-    start_urls = ["https://www.senscritique.com/nota2k/contacts"]
-    
-    def parse(self, response):
-        for membres in response.css('div.sc-74f141aa-0'):
-            link = membres.css('a::attr(href)')[1].get()
-            link_split = re.sub('/', '', link)
-            
-            yield {
-                    'username': membres.css("::text").get(),
-                    'link': link_split,
-                    'thmb': membres.css('a::attr(data-srcname)').get(),
-                    
-            }
+
 settings = get_project_settings()
 process = CrawlerProcess(settings)
 
